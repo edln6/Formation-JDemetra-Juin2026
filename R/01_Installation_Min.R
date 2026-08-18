6b96b7cb6e359c233aa0fae4ed9b4de7780c9f1e
Eulalie Delaune <168958470+edln6@users.noreply.github.com>
2026-08-18 14:10:30 +0200

    Update JAVA_HOME and add rjd3qr package
    Updated JAVA_HOME path for JDemetra+ to version 3.8.0 and added 'rjd3qr' package.

--- R/01_Installation_Min.R
+++ R/01_Installation_Min.R
@@ -6,11 +6,11 @@ install.packages(c(
     "rjd3tramoseats",
     "rjd3workspace",
     "rjwsacruncher",
-    "JDCruncheR",
+    "rjd3qr",
     "rjd3prodcution",
     "dplyr"
 
 ), repos = "https://nexus.insee.fr/repository/r-cran")
 
 # Etape JAVA_HOME
-Sys.setenv(JAVA_HOME = "Y:\\Logiciels\\JDemetraplus\\jdemetra-3.7.1\\nbdemetra\\jdk-21.0.7+6-jre")
+Sys.setenv(JAVA_HOME = "Y:\\Logiciels\\JDemetraplus\\jdemetra-3.8.0\\nbdemetra\\jdk-21.0.9+10-jre")
