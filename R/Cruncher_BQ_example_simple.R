46ece909a910413e813c3a01fafc553164236777
Eulalie Delaune <eulalie.delaune@eleve.ensai.fr>
2026-08-18 11:17:41 +0200

    change with rjd3qr

--- R/Cruncher_BQ_example_simple.R
+++ R/Cruncher_BQ_example_simple.R
@@ -6,7 +6,7 @@
 ## Chargement packages ---------------------------------------------------------
 
 library("rjwsacruncher")
-library("JDCruncheR")
+library("rjd3qr")
 library("rjd3workspace")
 
 
@@ -20,59 +20,71 @@ path_ws <- normalizePath("V:/Formations-Stats/CVS-CJO/Workspaces/Ipi_1.xml")
 ## Options et paramètres -------------------------------------------------------
 
 # Mettre votre chemin menant au cruncher
-cruncher_bin_path <- normalizePath("Y:/Logiciels/JDemetraplus/jwsacruncher-3.7.1/bin/")
+cruncher_bin_path <- normalizePath("Y:/Logiciels/JDemetraplus/jwsacruncher-3.8.0/bin/")
 cruncher_bin_path
 
 options(
     cruncher_bin_directory = cruncher_bin_path,
     is_cruncher_v3 = TRUE,
     default_matrix_item = c(
+        # FOR JVS:
+
         "span.start",
         "span.end",
+        "log",
         "span.n",
-        "arima",
-        "arima.mean",
-        "arima.p",
-        "arima.d",
-        "arima.q",
-        "arima.bp",
-        "arima.bd",
-        "arima.bq",
-        "m-statistics.m7",
-        "m-statistics.q",
-        "m-statistics.q-m2",
-        "diagnostics.out-of-sample.mean:2",
-        "diagnostics.out-of-sample.mse:2",
-        "diagnostics.fcast-outsample-mean:2",
-        "diagnostics.fcast-outsample-variance:2",
         "regression.nout",
-        "residuals.kurtosis:3",
-        "residuals.skewness:3",
+        "regression.ntd",
+        "m-statistics.m7",
+        "decomposition.seasonal-filters",
+        "decomposition.trend-filter",
+        "decomposition.d7-trend-filter",
+        "quality.summary",
+        "regression.lp",
+        "regression.leaster",
+        "diagnostics.seas-sa-ac1:3",
+        "diagnostics.seas-sa-ac1",
+        "diagnostics.seas-lin-combined",
+        "regression.out(*)",
+        "regression.td-ftest:3",
+        "residuals.lb:3",
+        "diagnostics.td-sa-last:2",
+        "diagnostics.seas-sa-f:2",
+        "arima.p", "arima.d", "arima.q", "arima.bp", "arima.bd", "arima.bq",
+        "m-statistics.q", "m-statistics.q-m2",
+
+        # FOR QR:
+
         "residuals.lb2:3",
+        "residuals.skewness:3",
+        "residuals.kurtosis:3",
+        "residuals.dh:3",
+        "residuals.doornikhansen:3",
         "diagnostics.seas-sa-qs:2",
         "diagnostics.seas-sa-qs",
-        "diagnostics.seas-sa-f:2",
         "diagnostics.seas-i-qs:2",
         "diagnostics.seas-i-qs",
         "diagnostics.seas-i-f:2",
-        "diagnostics.td-sa-last:2",
+        "diagnostics.seas-i-f",
         "diagnostics.td-i-last:2",
-        "residuals.lb:3",
-        "residuals.dh:3",
-        "residuals.doornikhansen:3"
-    )
+        "diagnostics.td-i-last",
+        "diagnostics.out-of-sample.mean:2",
+        "diagnostics.fcast-outsample-mean:2",
+        "diagnostics.out-of-sample.mse:2",
+        "diagnostics.fcast-outsample-variance:2"
+
+    ),
+    default_tsmatrix_series = c("y", "s", "sa", "t")
 )
 
 getOption("default_matrix_item")
-
 getOption("default_tsmatrix_series")
-options(default_tsmatrix_series = c("y", "y_f", "s", "s_f", "sa", "sa_f","y_lin"))
 
-# attention: faire une sauvegarde du WS avant de cruncher (ex: copie to repo cruncher)
+# ATTENTION: faire une sauvegarde du WS avant de cruncher (ex: copie to repo cruncher)
 # cruncher : mise à jour du workspace
 getwd()
 cruncher_and_param(
-    # Mettre le chemin menant à votre WS
+    # Mettre le chemin menant à VOTRE WS
     workspace = "V:/Formations-Stats/CVS-CJO/Workspaces/Ipi_1.xml",
     rename_multi_documents = TRUE,
     delete_existing_file = TRUE,
@@ -91,7 +103,7 @@ cruncher_and_param(
 demetra_path <- "V:/Formations-Stats/CVS-CJO/Workspaces/Ipi_1/Output/TD/demetra_m.csv"
 
 BQ_example <-extract_QR("V:/Formations-Stats/CVS-CJO/Workspaces/Ipi_1/Output/TD/demetra_m.csv") |>
-    compute_score(n_contrib_score = 3L) 
+    compute_score(n_contrib_score = 3L)
 
 # score_pond= formule
 
@@ -150,7 +162,7 @@ scores_example <- extract_score(BQ_example, weighted_score = TRUE)
 ## Export ----------------------------------------------------------------------
 
 # Ne pas enregistrer sous V:/Formations-Stats
-export_xlsx(
+write(
     x = BQ_example,
     file = "V:/Formations-Stats/CVS-CJO/BQ/BQ_Ipi1_TD.xlsx"
 )
